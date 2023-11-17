4658ebbce809acedea9ab599e0c971a136757f1f
Christian Lisboa (Tiki) <christian.moreno@tikiweb.com.br>
2023-11-17 15:28:08 -0300

    Add remove method to B-tree implementation

--- btree.py
+++ btree.py
@@ -65,6 +65,117 @@ class ArvoreB:
             z.filhos = y.filhos[t:]
             y.filhos = y.filhos[:t]
 
+    def remove(self, chave):
+        raiz = self.raiz
+        if chave in raiz.chaves:
+            if len(raiz.chaves) == 1 and len(raiz.filhos) > 1:
+                predecessor = self.get_predecessor(raiz)
+                raiz.chaves.remove(chave)
+                raiz.chaves.append(predecessor)
+                self.remove_predecessor(raiz, predecessor)
+            else:
+                self.remove_da_folha(raiz, chave)
+        else:
+            self.remove_recursivo(raiz, chave)
+
+    def remove_recursivo(self, x, chave):
+        i = 0
+        while i < len(x.chaves) and chave > x.chaves[i]:
+            i += 1
+
+        if i < len(x.chaves) and chave == x.chaves[i]:
+            if x.folha:
+                self.remove_da_folha(x, chave)
+            else:
+                self.remove_de_nao_folha(x, chave, i)
+        else:
+            if x.folha:
+                print(f"A chave {chave} não está na árvore.")
+            else:
+                filho = x.filhos[i]
+                if len(filho.chaves) == self.ordem - 1:
+                    self.preencher(filho, i)
+                self.remove_recursivo(filho, chave)
+
+    def remove_da_folha(self, x, chave):
+        x.chaves.remove(chave)
+
+    def remove_de_nao_folha(self, x, chave, i):
+        if len(x.filhos[i].chaves) >= self.ordem:
+            predecessor = self.get_predecessor(x.filhos[i])
+            x.chaves[i] = predecessor
+            self.remove_predecessor(x.filhos[i], predecessor)
+        elif len(x.filhos[i + 1].chaves) >= self.ordem:
+            sucessor = self.get_sucessor(x.filhos[i + 1])
+            x.chaves[i] = sucessor
+            self.remove_sucessor(x.filhos[i + 1], sucessor)
+        else:
+            self.merge(x, i)
+
+    def preencher(self, x, i):
+        if i != 0 and len(x.filhos[i - 1].chaves) >= self.ordem:
+            self.puxar_de_anterior(x, i)
+        elif i != len(x.filhos) - 1 and len(x.filhos[i + 1].chaves) >= self.ordem:
+            self.puxar_de_seguinte(x, i)
+        else:
+            if i != len(x.filhos) - 1:
+                self.merge(x, i)
+            else:
+                self.merge(x, i - 1)
+
+    def puxar_de_anterior(self, x, i):
+        filho = x.filhos[i]
+        irmao_anterior = x.filhos[i - 1]
+        filho.chaves.insert(0, x.chaves[i - 1])
+        x.chaves[i - 1] = irmao_anterior.chaves.pop()
+
+        if not filho.folha:
+            filho.filhos.insert(0, irmao_anterior.filhos.pop())
+
+    def puxar_de_seguinte(self, x, i):
+        filho = x.filhos[i]
+        irmao_seguinte = x.filhos[i + 1]
+        filho.chaves.append(x.chaves[i])
+        x.chaves[i] = irmao_seguinte.chaves.pop(0)
+
+        if not filho.folha:
+            filho.filhos.append(irmao_seguinte.filhos.pop(0))
+
+    def merge(self, x, i):
+        filho = x.filhos[i]
+        irmao_seguinte = x.filhos[i + 1]
+
+        filho.chaves.append(x.chaves[i])
+        filho.chaves.extend(irmao_seguinte.chaves)
+
+        if not filho.folha:
+            filho.filhos.extend(irmao_seguinte.filhos)
+
+        x.chaves.pop(i)
+        x.filhos.pop(i + 1)
+
+    def get_predecessor(self, x):
+        while not x.folha:
+            x = x.filhos[-1]
+        return x.chaves[-1]
+
+    def remove_predecessor(self, x, chave):
+        if x.folha:
+            x.chaves.remove(chave)
+        else:
+            self.remove_recursivo(x.filhos[-1], chave)
+
+    def get_sucessor(self, x):
+        while not x.folha:
+            x = x.filhos[0]
+        return x.chaves[0]
+
+    def remove_sucessor(self, x, chave):
+        if x.folha:
+            x.chaves.remove(chave)
+        else:
+            self.remove_recursivo(x.filhos[0], chave)
+
     def mostra_arvore(self):
         self._mostra_arvore(self.raiz)
 
@@ -84,11 +195,17 @@ if __name__ == "__main__":
     for chave in chaves:
         arvore_b.insere(chave)
 
-    chave_busca = 91
+    chave_busca = 6
     if arvore_b.busca(chave_busca):
         print(f'A chave {chave_busca} foi encontrada na árvore B.')
     else:
         print(f'A chave {chave_busca} não foi encontrada na árvore B.')
 
     print("\nÁrvore B:")
+    arvore_b.mostra_arvore()
+
+    chave_remover = 6
+    arvore_b.remove(chave_remover)
+
+    print(f"\nÁrvore B após remover a chave {chave_remover}:")
     arvore_b.mostra_arvore()
